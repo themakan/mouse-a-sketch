35a9a488e75c3729acab8c3c21283007591f54bb
themakan <themakan@gmail.com>
2015-05-07 20:41:24 -0700

    initial

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,5 +1,18 @@
 console.log("this is a sanity check");
+var currentColor;
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+
+  $( "td" ).mouseover(function() {
+  	
+  	// currentColor = document.getElementById("myColor").value;
+  	currentColor = $("input").val();
+  	// currentColor = $("#myColor").val();
+
+  	$(this).css("background-color", currentColor);
+  	
+});
+
 })
+
